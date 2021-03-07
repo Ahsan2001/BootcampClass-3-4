7a30372407931cfd833828222b1f9a14cf725800
Ahsan2001 <ahsan2sabir@gmail.com>
2021-03-07 15:39:48 +0500

    updated Files

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Message from './Components/message';
 function App() {
   
 const [count, setCount] = useState(0)
-return(
+const [isMorning, setMorning] = useState(true)
 
+return(
   <div>
       < Message numbers={count} />
+      <h1> Good {isMorning ? 'Morning' : 'Night'}</h1>
       <button onClick={()=> setCount (count + 1) }>Add My VALUE</button>
       <button onClick={()=> setCount (count - 1) }>Subtract My VALUE</button>
-
-
-</div>
+      <button onClick={()=>setMorning(!isMorning)}>Update Day</button>
+ </div>
 )
 }
 export default App
